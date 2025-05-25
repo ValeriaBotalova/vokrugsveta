5766b344479a7e02d6289a9c36a75d52172c3dbb
ValeriaBotalova <kaleriya.sych@mail.ru>
2025-05-25 19:12:22 +0300

    экспорт добавлен

--- admin_panel/urls.py
+++ admin_panel/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AdminToursView, ProcessTourView
+from .views import AdminToursView, ProcessTourView, ExportToursView
 
 app_name = 'admin_panel'
 
 urlpatterns = [
     path('', AdminToursView.as_view(), name='list'),
     path('process/<int:tour_id>/', ProcessTourView.as_view(), name='process'),
+    path('export/<str:format_type>/', ExportToursView.as_view(), name='export'),
 ]
--- admin_panel/views.py
+++ admin_panel/views.py
@@ -3,6 +3,9 @@ from django.contrib.auth.mixins import UserPassesTestMixin
 from django.shortcuts import render, redirect
 from core.models import Tours
 from django.http import HttpResponse
+from django.template.loader import render_to_string
+from openpyxl import Workbook
+from io import BytesIO
 
 def test_view(request):
     return HttpResponse("TEST OK")
@@ -34,3 +37,57 @@ class ProcessTourView(UserPassesTestMixin, View):
         
         tour.save()
         return redirect('admin_panel:list')
+
+class ExportToursView(UserPassesTestMixin, View):
+    def test_func(self):
+        return self.request.user.is_staff
+
+    def get(self, request, format_type):
+        tours = Tours.objects.all().order_by('-arrival_date')
+        
+        if format_type == 'xlsx':
+            return self.export_xlsx(tours)
+        elif format_type == 'html':
+            return self.export_html(tours)
+        else:
+            return HttpResponse("Unsupported format", status=400)
+
+    def export_xlsx(self, tours):
+        wb = Workbook()
+        ws = wb.active
+        ws.title = "Tours"
+
+        headers = ["ID", "Клиент", "Отель", "Дата прибытия", "Дата выезда", "Статус"]
+        ws.append(headers)
+
+        for tour in tours:
+            # Получаем полное имя через связанного пользователя
+            client_name = f"{tour.client.user.first_name} {tour.client.user.last_name}".strip()
+            if not client_name:  # Если имя и фамилия не заполнены
+                client_name = str(tour.client.user)
+                
+            ws.append([
+                tour.id,
+                client_name,  # Исправленная строка
+                str(tour.hotel.name),
+                tour.arrival_date,
+                tour.departure_date,
+                tour.get_status_display()
+            ])
+
+        output = BytesIO()
+        wb.save(output)
+        output.seek(0)
+
+        response = HttpResponse(
+            output.getvalue(),
+            content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        )
+        response['Content-Disposition'] = 'attachment; filename="tours_export.xlsx"'
+        return response
+
+    def export_html(self, tours):
+        html = render_to_string('admin_panel/tours_export.html', {'tours': tours})
+        response = HttpResponse(html, content_type='text/html')
+        response['Content-Disposition'] = 'attachment; filename="tours_export.html"'
+        return response
